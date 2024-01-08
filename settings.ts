d5ff1e2a2b1c68796fbd494898dfc494a31c6bb9
Alexander Sadovsky <alino.sadovsky@gmail.com>
2024-01-08 18:25:43 +0100

    fix typo

--- settings.ts
+++ settings.ts
@@ -2,7 +2,7 @@ export const Settings = {
     // To be customised by you
     targetLanguage: 'Czech',
     targetLanguageFileName: 'hu.json',  // Czech language code is 'hu' because the unnoficial Czech translation is rewritting hungarian.
-    folderLocationToSearchIn: `'/Users/admin/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS/Mods'`,
+    folderLocationToSearchIn: `/Users/admin/Library/Application Support/Steam/steamapps/common/Stardew Valley/Contents/MacOS/Mods`,
 
     // Probably you want to keep these as are
     sourceLanguage: 'English',
